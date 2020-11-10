62a66a76020f25c390acaa02282a87ef609b355d
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-10 23:04:35 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :first_name, :last_name, :first_reading, :last_reading, :birthday, presence: true
 end
