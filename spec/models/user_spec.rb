261f9b47d2ef55bbf6cadf2dbdcfd0cb97e056cb
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-11 01:38:53 +0900

    英数字混合のパスワードのテストの追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,6 +62,11 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
+    it "passwordは英数字混合でないと登録できない" do
+      user = User.new(nickname: "spec", email: "test@spec", first_name: "スペック", last_name:"太郎", first_reading:"スペック", last_reading:"タロウ", birthday:"1930-01-01", password: "aaaaaa", password_confirmation: "aaaaaa")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password Include both letters and numbers")
+    end
     it "password_confirmationが空では登録できない" do
       user = User.new(nickname: "spec", email: "test@spec", first_name: "スペック", last_name:"太郎", first_reading:"スペック", last_reading:"タロウ", birthday:"1930-01-01", password: "spec1234", password_confirmation: "")
       user.valid?
