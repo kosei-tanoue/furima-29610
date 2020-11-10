32f44024cad7bf20514fe176d8b1be572b29ff8d
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-11 01:09:08 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :first_name, :last_name, :first_reading, :last_reading, :birthday, presence: true
+  # validates :nickname, :first_name, :last_name, :first_reading, :last_reading, :birthday, presence: true
+
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_reading, :last_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
