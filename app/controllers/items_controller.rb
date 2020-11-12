40c8eb736e5c5190f42742771a8d75bd5a0be6e3
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-12 23:20:22 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
 class ItemsController < ApplicationController
+
   def index
   end
 
+  def new
+  end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explain_item, :category_id, :status_id, :delivery_fee_id, :place_id, :delivery_date_id).merge(user_id: current_user.id)
+  end
 end
