b19ca5b0294ef0b3f5e44b749cf79a5ecb7fd3fc
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-21 00:27:23 +0900

    購入画面へのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   resources :items do
+    member do
+      get :orders, to: 'orders#index'
+    end
   end
 end
