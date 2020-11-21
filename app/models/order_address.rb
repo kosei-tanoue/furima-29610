320ca4d2f0152884276f75fdcef86dc9e562c6e7
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-21 21:22:58 +0900

    郵便番号のバリデーションを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,10 +4,10 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :order_id, :building_name, :phone_number, :item_id, :user_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\d{3}-\d{4}/, message: "Input correctly"}
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city, :house_number
-    validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "Input only number"}
+    validates :phone_number, numericality: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "Input only number"}
   end
 
   def save
