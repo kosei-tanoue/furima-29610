21e2514d743a66ccddaffaf185a71391e0276148
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-21 19:25:58 +0900

    同じ記述をメソッドにまとめる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,15 @@
 class OrdersController < ApplicationController
   before_action :move_to_index, only: [:index]
+  before_action :item_find, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     @order_address = OrderAddress.new
   end
   
   def create
     # binding.pry
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       @order_address.save
@@ -24,13 +25,18 @@ class OrdersController < ApplicationController
   def order_address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :order_id, :building_name, :phone_number, :item_id, :user_id)
   end
-
-  def move_to_index
+  
+  def item_find
     @item = Item.find(params[:id])
-    if current_user.id == @item.user_id
+  end
+  
+  def move_to_index
+    item = Item.find(params[:id])
+    if current_user.id == item.user_id
       redirect_to root_path
     end
   end
+
 end
 
 
