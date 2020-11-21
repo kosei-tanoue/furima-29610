49290ab71b7c0e2b13d8fe70085cfc5996b4d58a
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-21 18:52:03 +0900

    order_controllerの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,27 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:id])
+    @order_address = OrderAddress.new
+  end
+  
+  def create
+    # binding.pry
+    @item = Item.find(params[:id])
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render "index"
+    end
+  end
+
+
+  private
+  
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :order_id, :building_name, :phone_number, :item_id, :user_id)
   end
 end
+
+
