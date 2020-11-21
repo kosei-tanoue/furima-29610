0bcfbbea12a7cdace3c3fdbef0f69a7f0ae7e2cd
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-21 21:23:22 +0900

    住所入力に関するFactoryBot

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code                     {1111111}
+    prefecture_id                   {2}
+    city                            {"なんとか市"}
+    house_number                    {"なんとか"}
+    building_name                   {"なんとかビル"}
+    phone_number                    {"09011111111"}
+    association :order
   end
 end
