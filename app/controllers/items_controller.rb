f13b9e29638eb7bbf104126a6f3db93fdd785289
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2020-11-25 19:08:53 +0900

    購入済みの商品に対する処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
+  before_action :not_access_sold_out, only: [:edit]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -50,5 +51,11 @@ class ItemsController < ApplicationController
   def item_find
     @item = Item.find(params[:id])
   end
-  
+
+  def not_access_sold_out
+    @item = Item.find(params[:id])
+    if @item.order.present?
+      redirect_to root_path
+    end
+  end
 end
